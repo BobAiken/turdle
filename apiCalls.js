7999c2dd09e277eea1f9b5c6d83de059fd8aafae
Robert Aiken <robertsiraaiken@gmail.com>
2022-12-15 13:28:33 -0600

    Add iterations2-5

--- apiCalls.js
+++ apiCalls.js
@@ -1,5 +1,3 @@
-let wordList;
-let gameData;
 
 function fetchData(dataset) {
   return fetch(`http://localhost:3001/api/v1/${dataset}`).then(response => response.json()).catch(error => console.log(`${dataset}`,error))
